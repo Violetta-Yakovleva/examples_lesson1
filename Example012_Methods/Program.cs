485ee9632dd47f255f33cf296d33fdcfcf4bc4d8
Violetta-Yakovleva <lysova-violetta@mail.ru>
2023-04-01 16:58:22 +0500

    Лекция 3

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -43,18 +43,121 @@
 
 // Вид 4 - метод что-то принимает и что-то возвращает.
 
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty; 
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty; 
 
-    while(i < count)
+//     while(i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// цикл FOR
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty; 
+//     for(int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//       return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+
+// Цикл в цикле
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for(int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Задача 1.Дан текст. В тексте нужно все пробелы заменить черточками, 
+// маленькие буквы "к" заменить большими "К", а большие "С" заменить маленькими "с".
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            +"ежели бы вас послали вместо нашего милого Винценгероде,"
+            +"вы бы взяли приступом согласие прусского короля."
+            +"Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"   
+//             012345
+// s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty; 
+//     int lenght = text.Length;
+//     for(int i = 0; i < lenght; i++)
+//     {
+//         if(text[i] == oldValue) 
+//         result = result + $"{newValue}";
+//         else 
+//         result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+// newText = Replace(newText, 'C', 'c');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+
+// Задача на сортировку методов выбора (метод минимакси).
+
+int[] arr = {1,5,4,3,2,6,7,1,1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
     {
-        result = result + text;
-        i++;
+        Console.Write($"{array[i]} ");
     }
-    return result;
+    Console.WriteLine();
 }
 
-string res = Method4(10, "z");
-Console.WriteLine(res);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])
+            minPosition = j; 
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
